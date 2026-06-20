26a97b7d155560cc5f6b356998ac235deac0ce60
jj-verse <jiyaa02@gmail.com>
2026-06-21 01:03:30 +0530

    fixed api key

--- utils/gemini_client.py
+++ utils/gemini_client.py
@@ -2,13 +2,10 @@ import os
 import google.generativeai as genai
 from dotenv import load_dotenv
 
-from dotenv import load_dotenv
-load_dotenv(dotenv_path=".env")
-print("API KEY =", os.getenv("GOOGLE_API_KEY"))
-print("CONFIG CALLED")
+load_dotenv()
 
 genai.configure(
-    api_key="AQ.Ab8RN6IQrxMkKAPaL6dcAnIsnck2sYO-lD64T03s6NN6dZCAqg"
+    api_key=os.getenv("GOOGLE_API_KEY")
 )
 
 model = genai.GenerativeModel("gemini-2.5-flash")
